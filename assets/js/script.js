1e7e941b4fc8667c904f1a1086a6cc5028d71943
codeyeskrish <yeskrish435@gmail.com>
2025-05-31 15:37:14 +0530

    same change

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Yeshwanth Krishna";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/images/hero.jpg");
         }
         else {
             document.title = "Come Back To Portfolio";
